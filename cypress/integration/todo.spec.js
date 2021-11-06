cdcaa9cb326afe5c3a8403d32c444c675c7daed2
Jujusp <jf.rubio@uniandes.edu.co>
2021-11-06 15:19:29 -0500

    integration test done

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,24 +1,36 @@
 describe('TODOS page', () => {
-  it('should add a new TODO', () => {
-    cy.visit('todo')
-      .get('input')
-      .type('new TODO')
-      .get('button[type=submit]')
-      .click()
-      .get('.list-group > .list-group-item:first > p')
-      .first()
-      .should('contain', '1. new TODO');
-  });
+    it('should add a new TODO', () => {
+        cy.visit('todo')
+            .get('input')
+            .type('new TODO')
+            .get('button[type=submit]')
+            .click()
+            .get('.list-group > .list-group-item:first > p')
+            .first()
+            .should('contain', '1. new TODO')
+    })
 
-  it('should toogle a previous TODO', () => {
-    cy.visit('/todo')
-      .get('input')
-      .type('new TODO')
-      .get('button[type=submit]')
-      .click()
-      .get('.list-group > .list-group-item:first > p')
-      .first()
-      .click()
-      .should('have.class', 'complete');
-  });
-});
+    it('should delete a TODO', () => {
+        cy.visit('/todo')
+            .get('input')
+            .type('new TODO')
+            .get('button[type=submit]')
+            .click()
+            .get('.list-group > .list-group-item:first > button')
+            .first()
+            .click()
+            .should('not.exist')
+    })
+
+    it('should toogle a previous TODO', () => {
+        cy.visit('/todo')
+            .get('input')
+            .type('new TODO')
+            .get('button[type=submit]')
+            .click()
+            .get('.list-group > .list-group-item:first > p')
+            .first()
+            .click()
+            .should('have.class', 'complete')
+    })
+})
